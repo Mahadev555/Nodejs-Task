b737e1fe5513bb77c0a66b56fcddb44fcc43c501
mahadev555 <maadevmanerikar@gmail.com>
2024-02-23 14:28:19 +0530

    aa

--- Login.js
+++ Login.js
@@ -1,5 +1,6 @@
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
 const passMatcvh = document.getElementById('passMatch')
+
 
 const array = [
     {
@@ -103,3 +104,63 @@ togglePasswordBtn.addEventListener('click', function () {
 function Sign(){
     window.location.href = 'index.html';
 }
+
+
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('pass')
+const passwordValidation = document.getElementById('passwordValidation');
+const emailValidation = document.getElementById('emailV');
+const emailRegex = /^[a-zA-Z]\w*([\.-]?\w+)*@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z]+\.){1}(com|in|org)))$/;
+
+//email typing validation
+emailInput.addEventListener('input', function () {
+    let emailValue = emailInput.value.trim();
+    emailInput.style.border = '1px solid red'
+    if (!emailValue) {
+        emailValidation.textContent = 'Email address is required';
+    } else if (emailValue.length > 60) {
+        // Trim the email to 30 characters
+        emailValue = emailValue.slice(0, 60);
+        emailInput.value = emailValue;
+        if (!emailRegex.test(emailValue)){
+            emailInput.style.border = '1px solid red'
+        }else{
+            emailInput.style.border = '1px solid #00ff55'
+        }   
+    } else if (!emailRegex.test(emailValue)) {
+        emailValidation.textContent = 'Invalid email address';
+    } else if (emailValue.split('.').length > 3) {
+        emailValidation.textContent = 'you can not enter more than 2 dot in your email id';
+    } else {
+        emailInput.style.border = '1px solid #00ff55'
+        emailValidation.textContent = '';
+    }
+});
+
+
+// password typing validation
+const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*[0-9]).{8,16}$/;
+
+passwordInput.addEventListener('input', function () {
+    let passwordValue = passwordInput.value.trim();
+    passwordInput.style.border = '1px solid red'
+
+    if (!passwordValue) {
+        passwordValidation.textContent = 'Password is required';
+    } else if (passwordValue.length > 16) {
+        passwordValue = passwordValue.slice(0, 16);
+        passwordInput.value = passwordValue; 
+        if (!passwordRegex.test(passwordValue)){
+            passwordInput.style.border = '1px solid red'
+        }else{
+            passwordInput.style.border = '1px solid #00ff55'
+        }
+    } else if (passwordValue.length < 8) {
+        passwordValidation.textContent = 'Password must be at least 8 characters';
+    } else if (!passwordRegex.test(passwordValue)) {
+        passwordValidation.textContent = 'Password must contain at least one special character, number, and capital letter';
+    } else {
+        passInput.style.border = '1px solid #00ff55'
+        passwordValidation.textContent = '';
+    }
+});
